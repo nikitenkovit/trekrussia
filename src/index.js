34f4876d6152f65d09411de5c06edac60d7ab7c5
Vitaliy <nelot@mail.ru>
2021-06-02 22:33:50 +0300

    закончил мобильную версию Ближайших туров

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import './styles/custom-font.css';
 import './styles/main.scss';
 
 import './js/header';
+import {ChiefSlider} from "./js/chiefSlider";
+
+// инициализация слайдеров
+document.addEventListener('DOMContentLoaded', function () {
+  const comingTours = document.getElementById('coming-tours');
+
+  if (comingTours) {
+    new ChiefSlider(comingTours, {
+      loop: true,
+      autoplay: true,
+      interval: 7000,
+      refresh: true,
+    });
+  }
+
+
+});
