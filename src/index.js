8e019ed772aeb24f1739ca4ad8c17f29b7e06240
Vitaliy <nelot@mail.ru>
2021-06-04 22:31:31 +0300

    мобильная версия Инструкторов

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ import {ChiefSlider} from "./js/chiefSlider";
 // инициализация слайдеров
 document.addEventListener('DOMContentLoaded', function () {
   const comingTours = document.getElementById('coming-tours');
+  const instructors = document.getElementById('instructors');
 
   if (comingTours) {
     new ChiefSlider(comingTours, {
       loop: true,
       autoplay: true,
       interval: 7000,
+      refresh: true,
+      swipe: true,
+    });
+  }
+
+  if (instructors) {
+    new ChiefSlider(instructors, {
+      loop: true,
+      autoplay: false,
       refresh: true,
       swipe: true,
     });
